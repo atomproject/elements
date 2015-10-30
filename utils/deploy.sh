23260631c5bb10ba08363db8035224ae706b0810
Ajinkya Tejankar <tejankarajinkya@gmail.com>
2015-10-30 18:44:59 +0530

    Add travis_base_url setting to config

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -19,4 +19,6 @@ git commit -m "Deploy to GitHub Pages"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+
+# !!!!! do not mess with this command unless you know what you're doing !!!!!
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
