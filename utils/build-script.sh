a4e9fb3861de5730c8bbe977a74446f2ce2fcfae
Ajinkya Tejankar <tejankarajinkya@gmail.com>
2015-11-04 15:43:54 +0530

    feat(elements pages): Add support for design considerations
    For a component copy the design-doc.md (the file containing
    design considerations text) from bower_components to elements
    directory. This file, once copied can be included using
    `include_relative` tag.
    
    [ci skip]

--- utils/build-script.sh
+++ utils/build-script.sh
@@ -1,5 +1,33 @@
 #!/usr/bin/env bash
 
+shopt -s extglob
+shopt -s globstar
+shopt -s nullglob
+
+copy_design_docs() {
+	rm elements/*/design-doc.md
+
+	for dir in elements/*
+	do
+		file_name="${dir##*/}"
+		component_name=$(grep 'component_name:' "$dir/$file_name.html")
+		component_name="${component_name##*:}"
+		component_name="${component_name## }"
+		component_name="${component_name%% }"
+		doc_path="bower_components/$component_name/design-doc.md"
+
+
+		if [[ -f "$doc_path" ]]; then
+			printf "%-50s --> %s" "$doc_path" "$dir/design-doc.md"
+			cp "$doc_path" "$dir"
+		fi
+	done
+
+	echo ""
+}
+
+copy_design_docs
+
 if [ "$1" == "--prod" ]
 then
 	jekyll build --config _config.yml,_config.prod.yml
