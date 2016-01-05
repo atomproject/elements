3792491ba0e4af381b5fc3e7afc91ee04e40e513
Ajinkya Tejankar <tejankarajinkya@gmail.com>
2016-01-05 10:59:42 +0530

    bug(build): Parameterize the email while deploying

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# TODO: clean up the files and find a better way of logging things
-# TODO: document the requirements git >2.6.3, bash >4.3 and node >4.1
 # TODO: assure user that github passwords aren't stored
 # TODO: deduce the current repo don't ask for it
 # TODO: run the task for creating `metadata.json`
@@ -40,41 +38,50 @@ data='{
 	"note": "'"$note"'"
 }'
 
-curl -s -u "$gh_uname:$gh_pass" -d "$data" https://api.github.com/authorizations >github-resp.log
-token="$(get_val '"token"' resp.log)"
+curl -s -u "$gh_uname:$gh_pass" -d "$data" https://api.github.com/authorizations >github-token-resp.log
+token="$(get_val '"token"' github-token-resp.log)"
 
 if [[ -z "$token" ]]
 then
 	echo ""
-	get_val '"message"' resp.log
+	get_val '"message"' github-token-resp.log
 	handle_logs_and_exit
 fi
-
 echo "Received GitHub token"
+
+curl -s -u "$gh_uname:$gh_pass" https://api.github.com/user >github-email-resp.log
+email="$(get_val '"email"' github-email-resp.log)"
+
+if [[ -z "$email" ]]
+then
+	echo ""
+	get_val '"message"' github-email-resp.log
+	handle_logs_and_exit
+fi
+echo "Received user email"
 
 data='{"github_token":"'"$token"'"}'
 
 curl -s -H "Accept: application/vnd.travis-ci.2+json" \
 	-H "Content-Type: application/json" \
 	-H "User-Agent: MyClient/1.0.0" \
-	-d "$data" https://api.travis-ci.org/auth/github >travis-resp.log
+	-d "$data" https://api.travis-ci.org/auth/github >travis-token-resp.log
 
-travis_token="$(get_val '"access_token"' travis-resp.log)"
+travis_token="$(get_val '"access_token"' travis-token-resp.log)"
 
 if [[ -z "$travis_token" ]]
 then
 	echo ""
 	echo "Login to Travis atleast once from the web interface if you haven't."
-	cat travis-resp.log
+	cat travis-token-resp.log
 	handle_logs_and_exit
 fi
 
 echo "Received Travis token"
 
-read -p "Repository Url: " repo_url
-repo_url="${repo_url#*//}"
-repo_url="${repo_url%/}"
-repo="${repo_url#*/}"
+repo_url="$(git remote show origin | grep 'github.com' | head -n 1)"
+repo_url="${repo_url##*github\.com[:/]}"
+repo="${repo_url%\.git}"
 
 curl -s -H "Accept: application/vnd.travis-ci.2+json" \
 	-H "User-Agent: MyClient/1.0.0" \
@@ -166,6 +173,28 @@ then
 	handle_logs_and_exit
 fi
 echo "Added CONFIG_GIST_URL environment variable"
+
+data='{
+	"env_var": {
+		"name": "USER_EMAIL",
+		"value": "'"$email"'",
+		"public": true
+	}
+}'
+
+curl -s -H "Accept: application/vnd.travis-ci.2+json" \
+	-H "Content-Type: application/json" \
+	-H "User-Agent: MyClient/1.0.0" \
+	-H "Authorization: token $travis_token" \
+	-d "$data" https://api.travis-ci.org/settings/env_vars?repository_id="$repo_id" >travis-env-email-resp.log
+
+if ! grep '"id":' travis-env-email-resp.log &>/dev/null
+then
+	echo ""
+	cat travis-env-email-resp.log
+	handle_logs_and_exit
+fi
+echo "Added USER_EMAIL environment variable"
 
 data='{
 	"settings": {
--- site_generator/deploy.sh
+++ site_generator/deploy.sh
@@ -8,7 +8,7 @@ git init
 
 # inside this git repo we'll pretend to be a new user
 git config user.name "Travis CI"
-git config user.email "tejankarajinkya@gmail.com"
+git config user.email "$USER_EMAIL"
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
